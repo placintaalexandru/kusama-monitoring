685c610d4c6b29657032f2b87c3fecfe36536d90
Alexandru Placinta <alexandru.placinta@nexthink.com>
2023-08-22 09:38:38 +0200

    Parse arguments

---
+++ src/logger.ts
@@ -0,0 +1,14 @@
+import { createLogger, Logger as LoggerW3f } from '@w3f/logger';
+
+export class LoggerSingleton {
+    private static instance: LoggerW3f
+
+    public static getInstance(level?: string): Logger {
+        if (!LoggerSingleton.instance) {
+            LoggerSingleton.instance = createLogger(level)
+        }
+        return LoggerSingleton.instance
+    }
+}
+
+export type Logger = LoggerW3f
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import {ApiPromise, WsProvider} from '@polkadot/api';
+import {loadConfig} from "./args";
 
 async function main() {
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const config = loadConfig();
+    console.log(config);
+
+    const wsProvider = new WsProvider(config.serviceConfig.endPoint);
     const api = await ApiPromise.create({ provider: wsProvider });
     console.log(api.genesisHash.toHex());
     await api.disconnect();
