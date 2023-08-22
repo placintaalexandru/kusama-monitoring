5e60e51df765c6b75d70dce686cb4dcf2754d1a4
Alexandru Placinta <alexandru.placinta@nexthink.com>
2023-08-22 22:10:55 +0200

    Create server

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import {ApiPromise, WsProvider} from '@polkadot/api';
 import {startCommand} from './args';
 import {LoggerSingleton} from './logger';
+import {startServer} from './server';
 
 async function main() {
     startCommand
-        .action(async (options) => {
+        .action(async options => {
             LoggerSingleton.setInstance(options.logLevel);
+            startServer(options);
             const logger = LoggerSingleton.getInstance();
             const wsProvider = new WsProvider(options.endpoint);
             const api = await ApiPromise.create({provider: wsProvider});
